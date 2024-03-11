6135f8f77af1c8b0bef4760940fdcc47c95743d1
Anar Kafkas <anarkafkas@gmail.com>
2024-03-11 22:55:21 +0300

    Add test definitions and test utils

--- src/generators/python/__tests__/generate-py.test.ts
+++ src/generators/python/__tests__/generate-py.test.ts
@@ -1,21 +1,13 @@
-import { resolve } from 'path';
-
-import { createDefinitionParser } from '../../../core/definition-parser';
+import { getSchemaForTestDefinition } from '../../../../test/util/get-schema-for-test-definition';
 import { createPythonGenerator } from '../PythonGeneratorImpl';
 
 describe('PythonGeneratorImpl', () => {
-  function getSchemaFor(definitionName: string) {
-    const parser = createDefinitionParser();
-    const pathToDefinition = resolve(__dirname, `definitions/${definitionName}.yml`);
-    return parser.parseDefinition(pathToDefinition);
-  }
-
   it('generates the correct output for flat schema', async () => {
     const generator = createPythonGenerator({
       indentation: 4,
       platform: 'py:firebase-admin:6',
     });
-    const schema = getSchemaFor('flat');
+    const schema = getSchemaForTestDefinition('flat');
     const output = await generator.generate(schema);
     expect(output.toString()).toMatchSnapshot();
   });
--- src/generators/ts/__tests__/generate-ts.test.ts
+++ src/generators/ts/__tests__/generate-ts.test.ts
@@ -1,21 +1,13 @@
-import { resolve } from 'path';
-
-import { createDefinitionParser } from '../../../core/definition-parser';
+import { getSchemaForTestDefinition } from '../../../../test/util/get-schema-for-test-definition';
 import { createTSGenerator } from '../TSGeneratorImpl';
 
 describe('TSGeneratorImpl', () => {
-  function getSchemaFor(definitionName: string) {
-    const parser = createDefinitionParser();
-    const pathToDefinition = resolve(__dirname, `definitions/${definitionName}.yml`);
-    return parser.parseDefinition(pathToDefinition);
-  }
-
   it('generates the correct output for flat schema', async () => {
     const generator = createTSGenerator({
       indentation: 4,
       platform: 'ts:firebase-admin:11',
     });
-    const schema = getSchemaFor('flat');
+    const schema = getSchemaForTestDefinition('flat');
     const output = await generator.generate(schema);
     expect(output.toString()).toMatchSnapshot();
   });
