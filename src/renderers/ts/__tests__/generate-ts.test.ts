306b667a7e9c657435d3b721257126ae38697ded
Anar Kafkas <anarkafkas@gmail.com>
2024-03-30 11:21:58 +0300

    TS renderer tests

---
+++ src/renderers/ts/__tests__/generate-ts.test.ts
@@ -0,0 +1,40 @@
+import { TSGeneration } from '../../../generators/ts/index.js';
+import { createTSRenderer } from '../_impl.js';
+
+describe('TSRendererImpl', () => {
+  it('correctly renders a TS generation', async () => {
+    const renderer = createTSRenderer({
+      rootFileName: 'models.py',
+      indentation: 4,
+      platform: 'ts:firebase-admin:11',
+    });
+
+    const generation: TSGeneration = {
+      type: 'ts',
+      declarations: [
+        {
+          type: 'alias',
+          modelName: 'Username',
+          modelType: {
+            type: 'string',
+          },
+        },
+        {
+          type: 'interface',
+          modelName: 'Dog',
+          modelType: {
+            type: 'object',
+            properties: [
+              { type: { type: 'string' }, name: 'name', docs: undefined, optional: false },
+              { type: { type: 'string' }, name: 'breed', docs: undefined, optional: false },
+            ],
+          },
+        },
+      ],
+    };
+
+    const result = await renderer.render(generation);
+
+    expect(result).toMatchSnapshot();
+  });
+});
