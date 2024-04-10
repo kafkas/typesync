7be15b34a39ca18dbc0b9b5455035295d8bf62cc
Anar Kafkas <anarkafkas@gmail.com>
2024-04-10 23:33:34 +0300

    Validation methods are not abstract anymore

--- src/generators/python/_schema.ts
+++ src/generators/python/_schema.ts
@@ -1,10 +1,3 @@
-import { InvalidAliasModelError, InvalidDocumentModelError } from '../../errors/invalid-model.js';
-import {
-  InvalidDiscriminantFieldError,
-  InvalidDiscriminatedUnionAliasVariantError,
-  MissingDiscriminantFieldError,
-  MissingDiscriminatedUnionAliasVariantError,
-} from '../../errors/invalid-schema-type.js';
 import { AbstractAliasModel, AbstractDocumentModel, AbstractSchema } from '../../schema/abstract.js';
 import type {
   AliasModel,
@@ -19,9 +12,6 @@ import type {
   TupleType,
 } from '../../schema/generic.js';
 import type { schema } from '../../schema/index.js';
-import { assertNever } from '../../util/assert.js';
-import { extractErrorMessage } from '../../util/extract-error-message.js';
-import { noop } from '../../util/misc.js';
 
 export type FlatType =
   | schema.types.Primitive
@@ -46,117 +36,6 @@ export type FlatModel = FlatAliasModel | FlatDocumentModel;
 export type FlatSchema = Schema<FlatAliasModel, FlatDocumentModel>;
 
 class FlatSchemaImpl extends AbstractSchema<FlatAliasModel, FlatDocumentModel> implements FlatSchema {
-  protected validateAliasModel(model: FlatAliasModel): void {
-    try {
-      this.validateType(model.type);
-    } catch (e) {
-      const message = extractErrorMessage(e);
-      throw new InvalidAliasModelError(model.name, message);
-    }
-  }
-
-  protected validateDocumentModel(model: FlatDocumentModel): void {
-    try {
-      this.validateType(model.type);
-    } catch (e) {
-      const message = extractErrorMessage(e);
-      throw new InvalidDocumentModelError(model.name, message);
-    }
-  }
-
-  protected validateType(t: schema.types.Type): void {
-    switch (t.type) {
-      case 'nil':
-      case 'string':
-      case 'boolean':
-      case 'int':
-      case 'double':
-      case 'timestamp':
-      case 'literal':
-      case 'enum':
-        return noop();
-      case 'tuple':
-        return this.validateTupleType(t);
-      case 'list':
-        return this.validateListType(t);
-      case 'map':
-        return this.validateMapType(t);
-      case 'object':
-        return this.validateObjectType(t);
-      case 'discriminated-union':
-        return this.validateDiscriminatedUnionType(t);
-      case 'simple-union':
-        return this.validateSimpleUnionType(t);
-      case 'alias':
-        return noop();
-      default:
-        assertNever(t);
-    }
-  }
-
-  protected validatePrimitiveType(_: schema.types.Primitive) {
-    noop();
-  }
-
-  protected validateTupleType(t: schema.types.Tuple) {
-    t.values.forEach(vt => this.validateType(vt));
-  }
-
-  protected validateListType(t: schema.types.List) {
-    this.validateType(t.of);
-  }
-
-  protected validateMapType(t: schema.types.Map) {
-    this.validateType(t.of);
-  }
-
-  protected validateObjectType(t: schema.types.Object) {
-    t.fields.forEach(field => {
-      this.validateType(field.type);
-    });
-  }
-
-  protected validateDiscriminatedUnionType(t: schema.types.DiscriminatedUnion) {
-    const { variants, discriminant } = t;
-    variants.forEach((variantType, variantIdx) => {
-      if (variantType.type === 'object') {
-        this.validateObjectType(variantType);
-        const { fields } = variantType;
-        const discriminantField = fields.find(f => f.name === discriminant);
-        if (discriminantField === undefined) {
-          throw new MissingDiscriminantFieldError(discriminant, variantIdx);
-        }
-        if (discriminantField.type.type !== 'literal' || typeof discriminantField.type.value !== 'string') {
-          throw new InvalidDiscriminantFieldError(variantIdx);
-        }
-      } else if (variantType.type === 'alias') {
-        const aliasModel = this.getAliasModel(variantType.name);
-        if (aliasModel === undefined) {
-          throw new MissingDiscriminatedUnionAliasVariantError(variantType.name);
-        }
-        if (aliasModel.type.type !== 'object') {
-          throw new InvalidDiscriminatedUnionAliasVariantError(variantType.name);
-        }
-        const { fields } = aliasModel.type;
-        const discriminantField = fields.find(f => f.name === discriminant);
-        if (discriminantField === undefined) {
-          throw new MissingDiscriminantFieldError(discriminant, variantType.name);
-        }
-        if (discriminantField.type.type !== 'literal' || typeof discriminantField.type.value !== 'string') {
-          throw new InvalidDiscriminantFieldError(variantType.name);
-        }
-      } else {
-        assertNever(variantType);
-      }
-    });
-  }
-
-  protected validateSimpleUnionType(t: schema.types.SimpleUnion) {
-    t.variants.forEach(vt => {
-      this.validateType(vt);
-    });
-  }
-
   public clone() {
     return this.cloneModels(new FlatSchemaImpl());
   }
--- src/schema/abstract.ts
+++ src/schema/abstract.ts
@@ -1,7 +1,17 @@
 import lodash from 'lodash';
 
+import { InvalidAliasModelError, InvalidDocumentModelError } from '../errors/invalid-model.js';
+import {
+  InvalidDiscriminantFieldError,
+  InvalidDiscriminatedUnionAliasVariantError,
+  MissingDiscriminantFieldError,
+  MissingDiscriminatedUnionAliasVariantError,
+} from '../errors/invalid-schema-type.js';
 import { assertNever } from '../util/assert.js';
+import { extractErrorMessage } from '../util/extract-error-message.js';
+import { noop } from '../util/misc.js';
 import type { AliasModel, DocumentModel } from './generic.js';
+import { schema } from './index.js';
 
 export abstract class AbstractAliasModel<T> {
   public readonly model = 'alias';
@@ -31,12 +41,12 @@ export abstract class AbstractDocumentModel<T> {
   }
 }
 
-export abstract class AbstractSchema<A extends AliasModel<unknown>, D extends DocumentModel<unknown>> {
-  protected readonly aliasModelsById: Map<string, A>;
-  protected readonly documentModelsById: Map<string, D>;
-
-  protected abstract validateAliasModel(model: A): void;
-  protected abstract validateDocumentModel(model: D): void;
+export abstract class AbstractSchema<
+  A extends AliasModel<schema.types.Type>,
+  D extends DocumentModel<schema.types.Type>,
+> {
+  private readonly aliasModelsById: Map<string, A>;
+  private readonly documentModelsById: Map<string, D>;
 
   public get aliasModels() {
     return Array.from(this.aliasModelsById.values());
@@ -128,5 +138,116 @@ export abstract class AbstractSchema<A extends AliasModel<unknown>, D extends Do
     if (dm !== undefined) {
       throw new Error(`The schema already has a '${model.name}' document model.`);
     }
+  }
+
+  private validateAliasModel(model: A): void {
+    try {
+      this.validateType(model.type);
+    } catch (e) {
+      const message = extractErrorMessage(e);
+      throw new InvalidAliasModelError(model.name, message);
+    }
+  }
+
+  private validateDocumentModel(model: D): void {
+    try {
+      this.validateType(model.type);
+    } catch (e) {
+      const message = extractErrorMessage(e);
+      throw new InvalidDocumentModelError(model.name, message);
+    }
+  }
+
+  private validateType(t: schema.types.Type): void {
+    switch (t.type) {
+      case 'nil':
+      case 'string':
+      case 'boolean':
+      case 'int':
+      case 'double':
+      case 'timestamp':
+      case 'literal':
+      case 'enum':
+        return noop();
+      case 'tuple':
+        return this.validateTupleType(t);
+      case 'list':
+        return this.validateListType(t);
+      case 'map':
+        return this.validateMapType(t);
+      case 'object':
+        return this.validateObjectType(t);
+      case 'discriminated-union':
+        return this.validateDiscriminatedUnionType(t);
+      case 'simple-union':
+        return this.validateSimpleUnionType(t);
+      case 'alias':
+        return noop();
+      default:
+        assertNever(t);
+    }
+  }
+
+  private validatePrimitiveType(_: schema.types.Primitive) {
+    noop();
+  }
+
+  private validateTupleType(t: schema.types.Tuple) {
+    t.values.forEach(vt => this.validateType(vt));
+  }
+
+  private validateListType(t: schema.types.List) {
+    this.validateType(t.of);
+  }
+
+  private validateMapType(t: schema.types.Map) {
+    this.validateType(t.of);
+  }
+
+  private validateObjectType(t: schema.types.Object) {
+    t.fields.forEach(field => {
+      this.validateType(field.type);
+    });
+  }
+
+  private validateDiscriminatedUnionType(t: schema.types.DiscriminatedUnion) {
+    const { variants, discriminant } = t;
+    variants.forEach((variantType, variantIdx) => {
+      if (variantType.type === 'object') {
+        this.validateObjectType(variantType);
+        const { fields } = variantType;
+        const discriminantField = fields.find(f => f.name === discriminant);
+        if (discriminantField === undefined) {
+          throw new MissingDiscriminantFieldError(discriminant, variantIdx);
+        }
+        if (discriminantField.type.type !== 'literal' || typeof discriminantField.type.value !== 'string') {
+          throw new InvalidDiscriminantFieldError(variantIdx);
+        }
+      } else if (variantType.type === 'alias') {
+        const aliasModel = this.getAliasModel(variantType.name);
+        if (aliasModel === undefined) {
+          throw new MissingDiscriminatedUnionAliasVariantError(variantType.name);
+        }
+        if (aliasModel.type.type !== 'object') {
+          throw new InvalidDiscriminatedUnionAliasVariantError(variantType.name);
+        }
+        const { fields } = aliasModel.type;
+        const discriminantField = fields.find(f => f.name === discriminant);
+        if (discriminantField === undefined) {
+          throw new MissingDiscriminantFieldError(discriminant, variantType.name);
+        }
+        if (discriminantField.type.type !== 'literal' || typeof discriminantField.type.value !== 'string') {
+          throw new InvalidDiscriminantFieldError(variantType.name);
+        }
+      } else {
+        assertNever(variantType);
+      }
+    });
+  }
+
+  private validateSimpleUnionType(t: schema.types.SimpleUnion) {
+    t.variants.forEach(vt => {
+      this.validateType(vt);
+    });
   }
 }
