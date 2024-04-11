f0d3e2af130ad3937b3a0b8098a331d5f16ea6a1
Anar Kafkas <anarkafkas@gmail.com>
2024-04-11 07:55:48 +0300

    Add tests for ordinal-suffix

--- src/util/ordinal-suffix.ts
+++ src/util/ordinal-suffix.ts
@@ -1,4 +1,10 @@
+/**
+ * @param i - Must be a nonnegative integer
+ */
 export function ordinalSuffixOf(i: number) {
+  if (i < 0 || !Number.isInteger(i)) {
+    throw new Error(`Expected nonnegative integer argument for 'ordinalSuffixOf()' but received ${i}.`);
+  }
   const j = i % 10;
   const k = i % 100;
   if (j === 1 && k !== 11) return i + 'st';
