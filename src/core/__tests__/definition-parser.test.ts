390955722270aa9fdec08c8bc58ec63037b35f49
Anar Kafkas <anarkafkas@gmail.com>
2024-04-16 21:42:20 +0300

    Add tests

--- src/core/__tests__/definition-parser.test.ts
+++ src/core/__tests__/definition-parser.test.ts
@@ -5,9 +5,17 @@ import { getDirName } from '../../util/fs.js';
 import { createDefinitionParser } from '../definition-parser.js';
 
 describe('definition-parser', () => {
-  it('correctly parses definition from a YAML file', () => {
+  it('correctly parses definition from YAML files', () => {
     const parser = createDefinitionParser();
-    const definitionGlobPattern = resolve(getDirName(import.meta.url), `./definitions/flat/*.yml`);
+    const definitionGlobPattern = resolve(getDirName(import.meta.url), `./definitions/yaml/*.yml`);
+    const filePaths = globSync(definitionGlobPattern);
+    const definition = parser.parseDefinition(filePaths);
+    expect(definition).toMatchSnapshot();
+  });
+
+  it('correctly parses definition from JSON files', () => {
+    const parser = createDefinitionParser();
+    const definitionGlobPattern = resolve(getDirName(import.meta.url), `./definitions/json/*.json`);
     const filePaths = globSync(definitionGlobPattern);
     const definition = parser.parseDefinition(filePaths);
     expect(definition).toMatchSnapshot();
