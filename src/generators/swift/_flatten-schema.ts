32c2a42f587c8a54358d908cc6853fab29e77baa
Anar Kafkas <anarkafkas@gmail.com>
2024-04-19 16:51:18 +0300

    Add declarations for document models

---
+++ src/generators/swift/_flatten-schema.ts
@@ -0,0 +1,173 @@
+import { schema } from '../../schema/index.js';
+import { assertNever } from '../../util/assert.js';
+import { extractDiscriminantValue } from '../../util/extract-discriminant-value.js';
+import { pascalCase } from '../../util/pascal-case.js';
+import {
+  FlatAliasModel,
+  FlatListType,
+  FlatMapType,
+  FlatObjectType,
+  FlatSchema,
+  FlatTupleType,
+  FlatType,
+  createFlatAliasModel,
+  createFlatDocumentModel,
+  createFlatSchema,
+} from './_schema.js';
+
+interface FlattenTupleTypeResult {
+  flattenedType: FlatTupleType;
+  extractedAliasModels: FlatAliasModel[];
+}
+
+interface FlattenListTypeResult {
+  flattenedType: FlatListType;
+  extractedAliasModels: FlatAliasModel[];
+}
+
+interface FlattenMapTypeResult {
+  flattenedType: FlatMapType;
+  extractedAliasModels: FlatAliasModel[];
+}
+
+interface FlattenObjectTypeResult {
+  flattenedType: FlatObjectType;
+  extractedAliasModels: FlatAliasModel[];
+}
+
+interface FlattenTypeResult {
+  flattenedType: FlatType;
+  extractedAliasModels: FlatAliasModel[];
+}
+
+/**
+ * Traverses a given schema and creates a new clone ensuring that all the schema types within it
+ * are expressible. Converts inline object and enum definitions to alias models where necessary.
+ *
+ * @returns A new schema object.
+ */
+export function flattenSchema(prevSchema: schema.Schema): FlatSchema {
+  function flattenTupleType(tupleType: schema.types.Tuple, aliasName: string): FlattenTupleTypeResult {
+    const resultsForValues = tupleType.elements.map((valueType, valueTypeIdx) =>
+      flattenType(valueType, `${aliasName}_${valueTypeIdx + 1}`)
+    );
+    const flattenedType: FlatTupleType = {
+      type: 'tuple',
+      elements: resultsForValues.map(res => res.flattenedType),
+    };
+    const extractedAliasModels = resultsForValues.map(res => res.extractedAliasModels).flat(1);
+    return { flattenedType, extractedAliasModels };
+  }
+
+  function flattenListType(listType: schema.types.List, aliasName: string): FlattenListTypeResult {
+    const resultForElementType = flattenType(listType.elementType, `${aliasName}Element`);
+    const flattenedType: FlatListType = {
+      type: 'list',
+      elementType: resultForElementType.flattenedType,
+    };
+    return { flattenedType, extractedAliasModels: resultForElementType.extractedAliasModels };
+  }
+
+  function flattenMapType(mapType: schema.types.Map, aliasName: string): FlattenMapTypeResult {
+    const resultForValueType = flattenType(mapType.valueType, `${aliasName}Value`);
+    const flattenedType: FlatMapType = {
+      type: 'map',
+      valueType: resultForValueType.flattenedType,
+    };
+    return { flattenedType, extractedAliasModels: resultForValueType.extractedAliasModels };
+  }
+
+  function flattenObjectType(objectType: schema.types.Object, aliasName: string): FlattenObjectTypeResult {
+    const resultsForFields = objectType.fields.map(field => {
+      const flattenResult = flattenType(field.type, `${aliasName}${pascalCase(field.name)}`);
+      return { field, flattenResult };
+    });
+    const flattenedType: FlatObjectType = {
+      type: 'object',
+      fields: resultsForFields.map(r => ({
+        docs: r.field.docs,
+        name: r.field.name,
+        optional: r.field.optional,
+        type: r.flattenResult.flattenedType,
+      })),
+    };
+    const extractedAliasModels = resultsForFields.map(r => r.flattenResult.extractedAliasModels).flat(1);
+    return { flattenedType, extractedAliasModels };
+  }
+
+  function flattenType(type: schema.types.Type, aliasName: string): FlattenTypeResult {
+    switch (type.type) {
+      case 'nil':
+      case 'string':
+      case 'boolean':
+      case 'int':
+      case 'double':
+      case 'timestamp':
+      case 'literal':
+      case 'alias':
+        return { flattenedType: type, extractedAliasModels: [] };
+      case 'enum': {
+        const name = aliasName;
+        const aliasModel = createFlatAliasModel({ name, docs: undefined, type });
+        const flattenedType: schema.types.Alias = { type: 'alias', name };
+        return { flattenedType, extractedAliasModels: [aliasModel] };
+      }
+      case 'tuple':
+        return flattenTupleType(type, aliasName);
+      case 'list':
+        return flattenListType(type, aliasName);
+      case 'map':
+        return flattenMapType(type, aliasName);
+      case 'object': {
+        const result = flattenObjectType(type, aliasName);
+        const name = aliasName;
+        const aliasModel = createFlatAliasModel({ name, docs: undefined, type: result.flattenedType });
+        const flattenedType: schema.types.Alias = { type: 'alias', name };
+        return { flattenedType, extractedAliasModels: [...result.extractedAliasModels, aliasModel] };
+      }
+      case 'discriminated-union':
+      case 'simple-union':
+        // TODO: Implement
+        throw new Error('Unimplemented');
+      default:
+        assertNever(type);
+    }
+  }
+
+  const newSchema = createFlatSchema();
+  const prevSchemaClone = prevSchema.clone();
+  const { aliasModels, documentModels } = prevSchemaClone;
+
+  aliasModels.forEach(aliasModel => {
+    let newModelType;
+    let extractedAliasModels: FlatAliasModel[] | undefined;
+
+    if (aliasModel.type.type === 'enum') {
+      newModelType = aliasModel.type;
+      extractedAliasModels = [];
+    } else if (aliasModel.type.type === 'object') {
+      ({ flattenedType: newModelType, extractedAliasModels } = flattenObjectType(aliasModel.type, aliasModel.name));
+    } else {
+      ({ flattenedType: newModelType, extractedAliasModels } = flattenType(aliasModel.type, aliasModel.name));
+    }
+    const flattenedModel = createFlatAliasModel({
+      name: aliasModel.name,
+      docs: aliasModel.docs,
+      type: newModelType,
+    });
+    // TODO: Check if this should run outside of this block
+    newSchema.addModelGroup([...extractedAliasModels, flattenedModel]);
+  });
+
+  documentModels.forEach(documentModel => {
+    const { flattenedType, extractedAliasModels } = flattenObjectType(documentModel.type, documentModel.name);
+    const flattenedModel = createFlatDocumentModel({
+      name: documentModel.name,
+      docs: documentModel.docs,
+      type: flattenedType,
+    });
+    newSchema.addModelGroup([...extractedAliasModels, flattenedModel]);
+  });
+
+  return newSchema;
+}
--- src/generators/swift/_impl.ts
+++ src/generators/swift/_impl.ts
@@ -1,11 +1,36 @@
+import { swift } from '../../platforms/swift/index.js';
 import { schema } from '../../schema/index.js';
+import { flatTypeToSwift } from './_converters.js';
+import { flattenSchema } from './_flatten-schema.js';
 import type { SwiftDeclaration, SwiftGeneration, SwiftGenerator, SwiftGeneratorConfig } from './_types.js';
 
 class SwiftGeneratorImpl implements SwiftGenerator {
   public constructor(private readonly config: SwiftGeneratorConfig) {}
 
   public generate(s: schema.Schema): SwiftGeneration {
+    const flattenedSchema = flattenSchema(s);
+    const { aliasModels, documentModels } = flattenedSchema;
+
     const declarations: SwiftDeclaration[] = [];
+
+    documentModels.forEach(model => {
+      // A Firestore document can be considered a 'struct' type
+      const swiftType: swift.Struct = {
+        type: 'struct',
+        properties: model.type.fields.map(f => ({
+          name: f.name,
+          type: flatTypeToSwift(f.type),
+          docs: f.docs,
+          optional: f.optional,
+        })),
+      };
+      declarations.push({
+        type: 'struct',
+        modelName: model.name,
+        modelType: swiftType,
+        modelDocs: model.docs,
+      });
+    });
 
     return { type: 'swift', declarations };
   }
