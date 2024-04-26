a2e15ddffa96f643d54b428bc21b09596a67992e
Anar Kafkas <36949216+kafkas@users.noreply.github.com>
2024-04-26 13:36:47 +0300

    style: improve GenerationSuccessful component (#134)
    * Change generation successful output
    
    * Make output path yellow

--- src/components/GenerationSuccessful.tsx
+++ src/components/GenerationSuccessful.tsx
@@ -12,11 +12,11 @@ export function GenerationSuccessful({ aliasModelCount, documentModelCount, path
   return (
     <Box flexDirection="column">
       <Text color="green">✔ Successfully generated type definitions for models.</Text>
-      <Text>✔ {pluralize('alias model', aliasModelCount, true)}</Text>
-      <Text>✔ {pluralize('document model', documentModelCount, true)}</Text>
+      <Text> - {pluralize('alias model', aliasModelCount, true)}</Text>
+      <Text> - {pluralize('document model', documentModelCount, true)}</Text>
       <Box>
         <Text>✔ Wrote output to: </Text>
-        <Text color="green">{pathToOutputFile}</Text>
+        <Text color="yellowBright">{pathToOutputFile}</Text>
       </Box>
     </Box>
   );
