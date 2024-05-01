e33ec6fcfe3247e9c87df7cb704d067058e17972
Anar Kafkas <anarkafkas@gmail.com>
2024-05-01 23:02:08 +0300

    Guards

---
+++ src/platforms/rules/_guards.ts
@@ -0,0 +1,25 @@
+import { assertNever } from '../../util/assert.js';
+import type { RulesDataType, Type } from './_types.js';
+
+export function isRulesDataType(t: Type): t is RulesDataType {
+  switch (t.type) {
+    case 'string':
+    case 'bool':
+    case 'float':
+    case 'int':
+    case 'timestamp':
+    case 'list':
+    case 'map':
+      return true;
+    case 'enum':
+    case 'object':
+    case 'any':
+    case 'literal':
+    case 'tuple':
+    case 'discriminated-union':
+    case 'simple-union':
+      return false;
+    default:
+      assertNever(t);
+  }
+}
