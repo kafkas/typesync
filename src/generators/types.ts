c9c90e95baf890db865761493eb38b84e9afd93c
Anar Kafkas <anarkafkas@gmail.com>
2024-05-02 22:03:33 +0300

    Remove more unused types

--- src/generators/types.ts
+++
@@ -1,6 +0,0 @@
-import { PythonGeneration } from './python/index.js';
-import { RulesGeneration } from './rules/index.js';
-import { SwiftGeneration } from './swift/index.js';
-import { TSGeneration } from './ts/index.js';
-
-export type Generation = PythonGeneration | RulesGeneration | SwiftGeneration | TSGeneration;
--- src/renderers/_types.ts
+++ src/renderers/_types.ts
@@ -1,12 +1,6 @@
-import type { Generation } from '../generators/index.js';
-
 export interface RenderedFile {
   /**
    * Content as string.
    */
   content: string;
-}
-
-export interface Renderer {
-  render(g: Generation): Promise<RenderedFile>;
 }
