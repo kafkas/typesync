74879e37d73ab75d303fcec88ad1e14885811b08
Anar Kafkas <anarkafkas@gmail.com>
2024-05-10 15:21:58 +0300

    Implement abstract schema factory

---
+++ src/schema/factory.ts
@@ -0,0 +1,71 @@
+import { AbstractAliasModel, AbstractDocumentModel, AbstractSchema } from './abstract.js';
+import {
+  AliasModel as AliasModelGeneric,
+  DocumentModel as DocumentModelGeneric,
+  Schema as SchemaGeneric,
+} from './generic.js';
+
+interface CreateAliasModelParams<AliasParameterType> {
+  name: string;
+  docs: string | null;
+  value: AliasParameterType;
+}
+
+interface CreateDocumentModelParams<DocumentParameterType> {
+  name: string;
+  docs: string | null;
+  type: DocumentParameterType;
+}
+
+export class AliasModel<AliasParameterType>
+  extends AbstractAliasModel<AliasParameterType>
+  implements AliasModelGeneric<AliasParameterType>
+{
+  public clone() {
+    return new AliasModel(this.name, this.docs, this.cloneType());
+  }
+}
+
+export class DocumentModel<DocumentParameterType>
+  extends AbstractDocumentModel<DocumentParameterType>
+  implements DocumentModelGeneric<DocumentParameterType>
+{
+  public clone() {
+    return new DocumentModel(this.name, this.docs, this.cloneType());
+  }
+}
+
+export class Schema<AliasParameterType, DocumentParameterType>
+  extends AbstractSchema<AliasModel<AliasParameterType>, DocumentModel<DocumentParameterType>>
+  implements SchemaGeneric<AliasModel<AliasParameterType>, DocumentModel<DocumentParameterType>>
+{
+  public clone() {
+    return this.cloneModels(new Schema<AliasParameterType, DocumentParameterType>());
+  }
+
+  public validateType(_t: unknown) {
+    // TODO: Implement
+  }
+}
+
+export class SchemaFactory<AliasParameterType, DocumentParameterType> {
+  public createSchema() {
+    return this.createSchemaWithModels([]);
+  }
+
+  public createSchemaWithModels(models: (AliasModel<AliasParameterType> | DocumentModel<DocumentParameterType>)[]) {
+    const s = new Schema<AliasParameterType, DocumentParameterType>();
+    s.addModelGroup(models);
+    return s;
+  }
+
+  public createAliasModel(params: CreateAliasModelParams<AliasParameterType>) {
+    const { name, docs, value } = params;
+    return new AliasModel(name, docs, value);
+  }
+
+  public createDocumentModel(params: CreateDocumentModelParams<DocumentParameterType>) {
+    const { name, docs, type } = params;
+    return new DocumentModel(name, docs, type);
+  }
+}
