556d0236c2ca0a3b763101d9f3c96e060334fe26
Anar Kafkas <anarkafkas@gmail.com>
2024-05-10 11:25:09 +0300

    Platform adjusted schema for ts

--- src/platforms/ts/_namespace.ts
+++ src/platforms/ts/_namespace.ts
@@ -1,3 +1,4 @@
+export * as schema from './schema/index.js';
 export * from './_expressions.js';
 export * from './_guards.js';
 export * from './_types.js';
---
+++ src/platforms/ts/schema/index.ts
@@ -0,0 +1,3 @@
+export * from './factory.js';
+export * from './impl.js';
+export type * as types from './types.js';
