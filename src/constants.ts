d34f2963aa3a63ba6b50a9cf3f455b142bc085b9
Anar Kafkas <anarkafkas@gmail.com>
2024-05-19 13:36:37 +0300

    Small fix

--- src/constants.ts
+++ src/constants.ts
@@ -7,7 +7,7 @@ export const RULES_VALIDATOR_NAME_PATTERN_PARAM = '{modelName}';
 /*
  * Default values
  */
-export const DEFAULT_TS_OBJECT_TYPE_FORMAT: TSObjectTypeFormat = 'interface';
+export const DEFAULT_TS_OBJECT_TYPE_FORMAT = 'interface' satisfies TSObjectTypeFormat;
 export const DEFAULT_TS_INDENTATION = 2;
 export const DEFAULT_TS_DEBUG = false;
 
@@ -28,7 +28,7 @@ export const DEFAULT_RULES_DEBUG = false;
 
 export const DEFAULT_GRAPH_START_MARKER = 'typesync-start';
 export const DEFAULT_GRAPH_END_MARKER = 'typesync-end';
-export const DEFAULT_GRAPH_ORIENTATION: SchemaGraphOrientation = 'horizontal';
+export const DEFAULT_GRAPH_ORIENTATION = 'horizontal' satisfies SchemaGraphOrientation;
 export const DEFAULT_GRAPH_DEBUG = false;
 
 export const DEFAULT_VALIDATE_DEBUG = false;
