726dc5606fb8417d0c3a02b1a6c307b52aab4281
Anar Kafkas <anarkafkas@gmail.com>
2024-05-25 13:48:37 +0300

    Init testing setup for rules

---
+++ security/rules.test.ts
@@ -0,0 +1,36 @@
+import { type RulesTestEnvironment, assertFails, initializeTestEnvironment } from '@firebase/rules-unit-testing';
+import { doc, setDoc } from 'firebase/firestore';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+function loadFirestoreRules(): string {
+  const pathToRules = resolve(__dirname, './firestore.rules');
+  return readFileSync(pathToRules, 'utf8');
+}
+
+describe('Security Rules', () => {
+  let testEnv: RulesTestEnvironment;
+
+  const userId = 'user123';
+  const userDocPath = `/users/${userId}`;
+
+  beforeAll(async () => {
+    testEnv = await initializeTestEnvironment({
+      firestore: {
+        rules: loadFirestoreRules(),
+      },
+    });
+  });
+
+  it('blocks create if the data object has incorrect shape', async () => {
+    const ctx = testEnv.unauthenticatedContext();
+    const userDocRef = doc(ctx.firestore(), userDocPath);
+    await expect(
+      assertFails(
+        setDoc(userDocRef, {
+          name: 'John',
+        })
+      )
+    ).resolves.toBeDefined();
+  });
+});
