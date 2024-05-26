239c17792ec6a4ee2e251f2f6e97a27dca69a1ee
Anar Kafkas <36949216+kafkas@users.noreply.github.com>
2024-05-26 11:21:14 +0300

    test: init Security Rules tests (#182)
    * Init testing setup for rules
    
    * Move to tests/security
    
    * Update tsconfig.build.json
    
    * Add projects
    
    * One more test
    
    * Clear db after each test
    
    * Update field
    
    * Test update not affecting read-only field
    
    * Test update affecting read-only field
    
    * Organize
    
    * Fix test paths
    
    * Update test scripts in package.json
    
    * Update tsconfig.test.json
    
    * Run new tests in ci
    
    * Install firebase-tools as dev dep
    
    * Test
    
    * Use global install
    
    * Remove global install
    
    * Use openjdk image
    
    * Add node
    
    * Prefix npx
    
    * Reenable filter

---
+++ tests/security/rules.test.ts
@@ -0,0 +1,97 @@
+import {
+  type RulesTestEnvironment,
+  assertFails,
+  assertSucceeds,
+  initializeTestEnvironment,
+} from '@firebase/rules-unit-testing';
+import { doc, setDoc, updateDoc } from 'firebase/firestore';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+function loadFirestoreRules(): string {
+  const pathToRules = resolve(__dirname, './firestore.rules');
+  return readFileSync(pathToRules, 'utf8');
+}
+
+describe('Security Rules', () => {
+  let testEnv: RulesTestEnvironment;
+
+  const userId = 'user123';
+  const userDocPath = `/users/${userId}`;
+  const projectId = 'project123';
+  const projectDocPath = `/projects/${projectId}`;
+
+  beforeAll(async () => {
+    testEnv = await initializeTestEnvironment({
+      firestore: {
+        rules: loadFirestoreRules(),
+      },
+    });
+  });
+
+  it('blocks create if the data object has incorrect shape', async () => {
+    const ctx = testEnv.unauthenticatedContext();
+    const userDocRef = doc(ctx.firestore(), userDocPath);
+    await expect(
+      assertFails(
+        setDoc(userDocRef, {
+          someField: 123,
+        })
+      )
+    ).resolves.toBeDefined();
+  });
+
+  it('allows create if the rules are relaxed', async () => {
+    const ctx = testEnv.unauthenticatedContext();
+    const projectDocRef = doc(ctx.firestore(), projectDocPath);
+    await expect(
+      assertSucceeds(
+        setDoc(projectDocRef, {
+          someField: 'abc',
+        })
+      )
+    ).resolves.toBeUndefined();
+  });
+
+  it('allows update if existing read-only fields are not affected', async () => {
+    const ctx = testEnv.unauthenticatedContext();
+    const userDocRef = doc(ctx.firestore(), userDocPath);
+    await setDoc(userDocRef, {
+      name: 'John Appleseed',
+      role: 'member',
+      created_at: new Date(),
+    });
+    await expect(
+      assertSucceeds(
+        updateDoc(userDocRef, {
+          name: 'James',
+        })
+      )
+    ).resolves.toBeUndefined();
+  });
+
+  it('blocks update if a read-only field is affected', async () => {
+    const ctx = testEnv.unauthenticatedContext();
+    const userDocRef = doc(ctx.firestore(), userDocPath);
+    await setDoc(userDocRef, {
+      name: 'John Appleseed',
+      role: 'member',
+      created_at: new Date(),
+    });
+    await expect(
+      assertFails(
+        updateDoc(userDocRef, {
+          role: 'admin',
+        })
+      )
+    ).resolves.toBeDefined();
+  });
+
+  afterEach(async () => {
+    await testEnv.clearFirestore();
+  });
+
+  afterAll(async () => {
+    await testEnv.cleanup();
+  });
+});
